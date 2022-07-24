64a2c9c02ed7cc8794509a30b72a8fa1c23cbef8
Aayush Joshi <aayushj8355@gmail.com>
2022-07-24 11:45:35 +0530

    Loops

---
+++ 4) Loops.py
@@ -0,0 +1,9 @@
+"""
+The provided code stub reads and integer, T, from STDIN. For all non-negative in
+i<n. print i2.
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(0,n):
+        print(i*i)
